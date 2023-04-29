dfe88b999db42c050ae2ab66c19a398565820dd8
DevWhkang <devwhkang@gmail.com>
2023-04-29 17:43:45 +0900

    Initial Commit

---
+++ Promise/PromiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromiseApp.swift
+//  Promise
+//
+//  Created by dylan on 2023/04/29.
+//
+
+import SwiftUI
+
+@main
+struct PromiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
