d113d324a2113a47a49d310bd3a7d5a9269f3dac
DevWhkang <devwhkang@gmail.com>
2023-06-06 00:14:04 +0900

    Remove. alert

--- Promise/Views/ViewController.swift
+++ Promise/Views/ViewController.swift
@@ -12,17 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        showConfigAlert()
-    }
-    
-    func showConfigAlert() {
-        let alert = UIAlertController(title: "Config Values", message: "BUILD_ENV: \(Config.buildENV), APP_ENV: \(Config.appENV), API_URL: \(Config.apiURL)", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
-        self.present(alert, animated: true, completion: nil)
     }
 }
 
