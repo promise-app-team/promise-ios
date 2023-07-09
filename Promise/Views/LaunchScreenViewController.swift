89d2034168e9b2d86c289f1713abd4c5d5d354e7
zzee22su <patrickstar0910@gmail.com>
2023-07-09 15:39:10 +0900

    Fix. logo image autolayout

--- Promise/Views/LaunchScreenViewController.swift
+++ Promise/Views/LaunchScreenViewController.swift
@@ -22,42 +22,42 @@ class LaunchScreenViewController: UIViewController {
         return imageView
     }()
     
-    private lazy var lottieAnimationView: LottieAnimationView = {
-        let animationView = LottieAnimationView(name: "_primary-gps-lottie")
-            animationView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
-            animationView.layer.shadowOpacity = 1
-            animationView.layer.shadowRadius = 4
-            animationView.layer.shadowOffset = CGSize(width: 4, height: 4)
-        return animationView
-    }()
+//    private lazy var lottieAnimationView: LottieAnimationView = {
+//        let animationView = LottieAnimationView(name: "_primary-gps-lottie")
+//            animationView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+//            animationView.layer.shadowOpacity = 1
+//            animationView.layer.shadowRadius = 4
+//            animationView.layer.shadowOffset = CGSize(width: 4, height: 4)
+//        return animationView
+//    }()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         makeUI()
-        lottieAnimationView.play()
+//        lottieAnimationView.play()
     }
     
     func makeUI() {
         launchScreenView.addSubview(logoImageView)
-        launchScreenView.addSubview(lottieAnimationView)
+//        launchScreenView.addSubview(lottieAnimationView)
         view.addSubview(launchScreenView)
         
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        lottieAnimationView.translatesAutoresizingMaskIntoConstraints = false
+//        lottieAnimationView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             // 프로미스앱 로고 이미지 오토레이아웃 설정
-            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logoImageView.widthAnchor.constraint(equalToConstant: 159.81),
-            logoImageView.heightAnchor.constraint(equalToConstant: 39.33),
-            logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 406.83),
+            logoImageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 225),
+            logoImageView.heightAnchor.constraint(equalToConstant: 59),
+            logoImageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
 
             // 프로미스앱 로고 로티 이미지 오토레이아웃 설정
-            lottieAnimationView.leadingAnchor.constraint(equalTo: logoImageView.trailingAnchor, constant: -124),
-            lottieAnimationView.widthAnchor.constraint(equalToConstant: 80),
-            lottieAnimationView.heightAnchor.constraint(equalToConstant: 80),
-            lottieAnimationView.topAnchor.constraint(equalTo: logoImageView.topAnchor, constant: -81)
+//            lottieAnimationView.leadingAnchor.constraint(equalTo: logoImageView.trailingAnchor, constant: -124),
+//            lottieAnimationView.widthAnchor.constraint(equalToConstant: 80),
+//            lottieAnimationView.heightAnchor.constraint(equalToConstant: 80),
+//            lottieAnimationView.topAnchor.constraint(equalTo: logoImageView.topAnchor, constant: -81)
         ])
     }
 }
