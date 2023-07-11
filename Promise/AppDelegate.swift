69496b7ca482f1d16701c75fb326e0efcfc5ed2a
zzee22su <patrickstar0910@gmail.com>
2023-07-11 18:05:14 +0900

    Feat. Push Notification on Background

--- Promise/AppDelegate.swift
+++ Promise/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+//        UIApplication.shared.registerForRemoteNotifications()
+        registerForPushNotifications()
         return true
     }
 
@@ -30,7 +33,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func registerForPushNotifications() {
+        // 알림을 포함 앱의 모든 알림 관련 활동 처리.
+        UNUserNotificationCenter.current()
+        // 알림 유형 설정가능 (alert, sound, badge)
+            .requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
+                print("Permission granted: \(granted)") //인증결과 표시
+                guard granted else { return }
+                self.getNotificationSettings()
+            }
+    }
+    
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                //APNs에 등록
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+        
+    }
+    
+    //registerForPushNotifications가 성공할 때마다 자동으로 호출되는 함수.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
 
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
 
 }
 
