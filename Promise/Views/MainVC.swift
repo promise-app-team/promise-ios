274cc26c1ca193f4db951f0703cfe1162ab8c3ed
DevWhkang <devwhkang@gmail.com>
2023-07-14 22:23:41 +0900

    Update. openapi

--- Promise/Views/MainVC.swift
+++ Promise/Views/MainVC.swift
@@ -8,9 +8,41 @@
 import UIKit
 
 final class MainVC: UIViewController {
+    private let tokenLabel = {
+        let label = UILabel()
+        let token = UserService.shared.getAccessToken()
+        
+        label.text = "Your Access Token is:\n\n\(token!)"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textColor = UIColor(red: 140/255, green: 140/255, blue: 140/255, alpha: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private func setupAutoLayout() {
+        NSLayoutConstraint.activate([
+            tokenLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            tokenLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            tokenLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // main
+        configureMainView()
+        render()
+    }
+    
+    func configureMainView() {
+        view.backgroundColor = .white
+    }
+    
+    private func render() {
+        [tokenLabel].forEach { view.addSubview($0) }
+        setupAutoLayout()
     }
 }
 
