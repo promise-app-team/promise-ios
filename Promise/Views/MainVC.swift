c5991e7e19cdcbd5a4275adf36494792a0f30845
DevWhkang <devwhkang@gmail.com>
2023-07-15 19:59:49 +0900

    Add. main vc

--- Promise/Views/MainVC.swift
+++ Promise/Views/MainVC.swift
@@ -22,7 +22,26 @@ final class MainVC: UIViewController {
         return label
     }()
     
+    private let mainPageLabel = {
+        let label = UILabel()
+        
+        label.text = "Main page"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = UIColor(red: 1, green: 0.41, blue: 0.3, alpha: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private func setupAutoLayout() {
+        NSLayoutConstraint.activate([
+            mainPageLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            mainPageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            mainPageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+        ])
+        
         NSLayoutConstraint.activate([
             tokenLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
             tokenLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
@@ -41,7 +60,7 @@ final class MainVC: UIViewController {
     }
     
     private func render() {
-        [tokenLabel].forEach { view.addSubview($0) }
+        [mainPageLabel, tokenLabel].forEach { view.addSubview($0) }
         setupAutoLayout()
     }
 }
