c4e13a66dba28a25bdabc015f70d23ba844bc165
zzee22su <patrickstar0910@gmail.com>
2023-07-22 11:35:46 +0900

    Add. Navigation Bar

---
+++ Promise/Views/SettingsVC.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsVC.swift
+//  Promise
+//
+//  Created by zzee22su on 2023/07/22.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeNavigationBar()
+    }
+    
+    func makeNavigationBar() {
+        view.backgroundColor = .white
+
+        title = "설정"
+        navigationController?.navigationBar.titleTextAttributes = [
+                    .font: UIFont.systemFont(ofSize: 16, weight: .regular)]
+
+        // MARK: 이미지 색상 변경
+        let backButtonImage = UIImage(named: "Chevron left")?.withRenderingMode(.alwaysOriginal)
+        let backButton = UIBarButtonItem(image: backButtonImage, style: .plain, target: self, action: #selector(backBtnTapped))
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    @objc private func backBtnTapped() {
+        print("뒤로가기 버튼")
+    }
+}
