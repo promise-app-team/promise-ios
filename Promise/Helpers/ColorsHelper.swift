36759b0693f1d2ff7a62bc4b12b9506848517df0
zzee22su <patrickstar0910@gmail.com>
2023-07-23 15:03:26 +0900

    Add. ColorsHelper

---
+++ Promise/Helpers/ColorsHelper.swift
@@ -0,0 +1,27 @@
+//
+//  ColorsHelper.swift
+//  Promise
+//
+//  Created by jisu on 2023/07/23.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    //Primary
+    static let p_D10 = UIColor(named: "p_D10")
+    static let p_p = UIColor(named: "p_p")
+    static let p_D30 = UIColor(named: "p_D30")
+    static let p_L50 = UIColor(named: "p_L50")
+    static let p_L80 = UIColor(named: "p_L80")
+    static let p_L90 = UIColor(named: "p_L90")
+    static let p_L97 = UIColor(named: "p_L97")
+    
+    //Secondary
+    static let s_D10 = UIColor(named: "s_D10")
+    static let s_p = UIColor(named: "s_p")
+    static let s_L50 = UIColor(named: "s_L50")
+    static let s_L90 = UIColor(named: "s_L90")
+    static let s_L97 = UIColor(named: "s_L97")
+}
