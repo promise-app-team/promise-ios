ae9c046db55ded8a0aad8c83fc8481d0504009db
doshkor <dosh.kor@gmail.com>
2023-09-25 12:55:10 +0900

    Refactor. location service

--- Promise/Services/LocationService.swift
+++ Promise/Services/LocationService.swift
@@ -9,28 +9,69 @@ import CoreLocation
 
 class LocationService {
     
+    // MARK: - Static property
+    
     static let shared = LocationService()
+    
+    // MARK: - Public property
+    
+    var authorizationStatus: CLAuthorizationStatus {
+        if #available(iOS 14, *) {
+            return manager.authorizationStatus
+        }
+        return CLLocationManager.authorizationStatus()
+    }
+    
+    // MARK: - Private property
     
     private let manager = CLLocationManager()
     
-    init() {
-        manager.desiredAccuracy = kCLLocationAccuracyBest
+    // MARK: - Initializer
+    
+    private init() {
+        configureManager()
+    }
+    
+    // MARK: - Public function
+    
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func startMonitoring(
+        _ latitude: CLLocationDegrees,
+        _ longitude: CLLocationDegrees,
+        radius: CLLocationDistance,
+        identifier: String) {
+            
+            let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let region = CLCircularRegion(center: center, radius: radius, identifier: identifier)
+            manager.startMonitoring(for: region)
+        }
+    
+    func stop() {
+        manager.stopUpdatingLocation()
+    }
+    
+    func backgroundUpdateOn() {
         manager.allowsBackgroundLocationUpdates = true
         manager.pausesLocationUpdatesAutomatically = false
     }
     
-    func start() {
-        manager.requestAlwaysAuthorization()
-        manager.startUpdatingLocation()
+    func backgroundUpdateOff() {
+        manager.allowsBackgroundLocationUpdates = false
+        manager.pausesLocationUpdatesAutomatically = true
     }
     
-    func startMonitoring(lat latitude: CLLocationDegrees, lon longitude: CLLocationDegrees, radius: CLLocationDistance, identifier: String) {
-        let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let region = CLCircularRegion(center: center, radius: radius, identifier: identifier)
-        manager.startMonitoring(for: region)
-    }
+    // MARK: - Private function
     
-    func stop() {
-        manager.stopUpdatingLocation()
+    private func configureManager() {
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if authorizationStatus == .authorizedAlways {
+            manager.pausesLocationUpdatesAutomatically = false
+            manager.allowsBackgroundLocationUpdates = true
+        }
     }
 }
