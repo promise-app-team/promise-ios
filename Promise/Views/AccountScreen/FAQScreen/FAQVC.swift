e4011bbe60428df7dd7e8b9dba196338a768e907
zzee22su <patrickstar0910@gmail.com>
2023-10-29 22:46:06 +0900

    Add. headerView for subscreen

--- Promise/Views/AccountScreen/FAQScreen/FAQVC.swift
+++ Promise/Views/AccountScreen/FAQScreen/FAQVC.swift
@@ -9,25 +9,18 @@ import UIKit
 
 class FAQVC: UIViewController {
     
-    lazy var label: UILabel = {
-        let label = UILabel()
-        label.text = "자주 묻는 질문"
-        label.textColor = .black
-        label.font = UIFont.pretendard(style: .H1_B)
-        label.textAlignment = .center
-        return label
+    //헤더
+     lazy var headerView: HeaderView = {
+        let navigationController = self.navigationController
+        let title = L10n.Account.faq
+        let headerView = HeaderView(navigationController: navigationController, title: title)
+        return headerView
     }()
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureAccountVC()
         render()
-        
-        if #available(iOS 11.0, *) {
-                additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-                view.backgroundColor = .white
-            }
     }
            
     func configureAccountVC() {
@@ -35,17 +28,17 @@ class FAQVC: UIViewController {
     }
     
     func render() {
-        [label].forEach { view.addSubview($0) }
-        [label].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        [headerView].forEach { view.addSubview($0) }
+        [headerView].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
         setupAutoLayout()
     }
     
     func setupAutoLayout() {
         NSLayoutConstraint.activate([
-            label.widthAnchor.constraint(equalToConstant: 253),
-            label.heightAnchor.constraint(equalToConstant: 36),
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)])
+            headerView.heightAnchor.constraint(equalToConstant: 56),
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)])
     }
-    
+
 }
