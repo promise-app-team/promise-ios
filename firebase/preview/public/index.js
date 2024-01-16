6077e123375a6389b3447afe06ada3789afde8f4
kwh <kwh@kwhs-MacBook-Pro.local>
2024-01-16 21:34:58 +0900

    fix. redirect system

---
+++ firebase/preview/public/index.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const isIOS = isIOSDevice();
+
+    if (isIOS) {
+        const path = window.location.pathname;
+        const link = `https://global-song-391605.web.app/${path}`;
+
+        const isKakaoTalk = isKakaoTalkInAppBrowser();
+        if(isKakaoTalk) {
+            window.location.href = 'kakaotalk://web/openExternal?url='+encodeURIComponent(link);
+            return;
+        }
+        
+        assignGetPromiseBtnHandler(link)
+    }
+});
+
+function assignGetPromiseBtnHandler(link) {
+    const element = document.querySelector('.btn-get-promise');
+    element.onclick = () => {
+        window.location.href = link;
+    };
+};
+
+function isIOSDevice() {
+    return /iPhone|iPad/i.test(navigator.userAgent);
+};
+
+function isKakaoTalkInAppBrowser() {
+    return navigator.userAgent.toLowerCase().match(/kakaotalk/i);
+};
