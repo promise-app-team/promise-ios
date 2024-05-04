984543f05917978686c687b4c8d55c461596e435
DevWhkang <devwhkang@gmail.com>
2024-05-04 23:31:43 +0900

    Fix. initial focused cell border

--- Promise/Views/MainScreen/MainVC.swift
+++ Promise/Views/MainScreen/MainVC.swift
@@ -478,6 +478,7 @@ extension MainVC: UICollectionViewDataSource, UICollectionViewDelegate {
         
         // MARK: 최초에 한 번만 실행, cell 재사용시는 focusRatio가 initRaio와 다르기 때문에 실행되지 않고 layoutAttributesForElements 부분이 실행됨.
         if indexPath.row == 0,
+           mainVM.currentFocusedPromiseIndexPath == nil,
            let initFocusRatio = focusRatioInfo.0,
            let focusRatio = focusRatioInfo.1,
            initFocusRatio == focusRatio
