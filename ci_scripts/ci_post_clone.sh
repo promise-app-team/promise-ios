a15ae706cacfad8bf1f95fd4a17ced8da6e65927
DevWhkang <devwhkang@gmail.com>
2024-05-26 18:04:37 +0900

    Add. auto increase build number for ci/cd

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,9 +8,13 @@
 
 #!/usr/bin/env bash
 
+# Cocoapods
 brew install cocoapods
 pod install
 
+# OpenAPIGenerator
 set -euo pipefail
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 
+# Automatically increase build number (Special new build number setting: xcrun agvtool new-version -all 1)
+xcrun agvtool next-version -all
